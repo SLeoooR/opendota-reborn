83e588b480089bccbf070d737e63b68b912c3aa1
SLeoooR <sreganit@gbox.adnu.edu.ph>
2022-04-27 17:58:51 +0800

    Added expanding tile views in the profile settings page.

--- app/src/main/java/com/scottandmarc/opendotareborn/app/presentation/dashboard/settings/SettingsFragment.kt
+++ app/src/main/java/com/scottandmarc/opendotareborn/app/presentation/dashboard/settings/SettingsFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
 import com.scottandmarc.opendotareborn.R
 import com.scottandmarc.opendotareborn.databinding.FragmentSettingsBinding
 import com.scottandmarc.opendotareborn.di.DependencyInjector
@@ -39,6 +41,62 @@ class SettingsFragment : Fragment(), SettingsContract.View {
     private fun initViews() {
         binding.btnChangeSteamFriendCode.setOnClickListener {
             presenter.onChangeCodeBtnClick()
+        }
+
+        binding.llFAQOneTitle.setOnClickListener {
+            if (binding.tvFAQOne.visibility == View.GONE) {
+                TransitionManager.beginDelayedTransition(
+                    binding.llFAQOne,
+                    AutoTransition()
+                )
+                binding.tvFAQOne.visibility = View.VISIBLE
+                binding.ivFAQOneIcon.setImageResource(R.drawable.ic_expand_less)
+            } else {
+                binding.tvFAQOne.visibility = View.GONE
+                binding.ivFAQOneIcon.setImageResource(R.drawable.ic_expand_more)
+            }
+        }
+
+        binding.llFAQTwoTitle.setOnClickListener {
+            if (binding.tvFAQTwo.visibility == View.GONE) {
+                TransitionManager.beginDelayedTransition(
+                    binding.llFAQTwo,
+                    AutoTransition()
+                )
+                binding.tvFAQTwo.visibility = View.VISIBLE
+                binding.ivFAQTwoIcon.setImageResource(R.drawable.ic_expand_less)
+            } else {
+                binding.tvFAQTwo.visibility = View.GONE
+                binding.ivFAQTwoIcon.setImageResource(R.drawable.ic_expand_more)
+            }
+        }
+
+        binding.llFAQThreeTitle.setOnClickListener {
+            if (binding.tvFAQThree.visibility == View.GONE) {
+                TransitionManager.beginDelayedTransition(
+                    binding.llFAQThree,
+                    AutoTransition()
+                )
+                binding.tvFAQThree.visibility = View.VISIBLE
+                binding.ivFAQThreeIcon.setImageResource(R.drawable.ic_expand_less)
+            } else {
+                binding.tvFAQThree.visibility = View.GONE
+                binding.ivFAQThreeIcon.setImageResource(R.drawable.ic_expand_more)
+            }
+        }
+
+        binding.llFAQFourTitle.setOnClickListener {
+            if (binding.tvFAQFour.visibility == View.GONE) {
+                TransitionManager.beginDelayedTransition(
+                    binding.llFAQFour,
+                    AutoTransition()
+                )
+                binding.tvFAQFour.visibility = View.VISIBLE
+                binding.ivFAQFourIcon.setImageResource(R.drawable.ic_expand_less)
+            } else {
+                binding.tvFAQFour.visibility = View.GONE
+                binding.ivFAQFourIcon.setImageResource(R.drawable.ic_expand_more)
+            }
         }
     }
 
